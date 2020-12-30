fce5fb3150eb807fd65ab8761373d2c8c4289ef8
Kate Shijie Xu <s.k.xu@berkeley.edu>
2020-12-30 12:25:08 -0800

    Updated UI: Toggle editing of raw text and romaji

--- public/script.js
+++ public/script.js
@@ -1,8 +1,23 @@
-function copy() {
+function copy(target) {
   const range = document.createRange();
-  range.selectNode(document.getElementById('romaji'));
+  range.selectNode(document.getElementById(target));
   window.getSelection().removeAllRanges();
   window.getSelection().addRange(range);
   document.execCommand('copy');
   window.getSelection().removeAllRanges();
+}
+
+function edit(id) {
+  const target = '#' + id;
+  const button = id == 'romaji' ?
+  	$('button#romaji-edit-button') :
+  	$('button#text-edit-button');
+
+  if (button.text() == 'Disable Edit') {
+  	$(button).text('Enable Edit');
+  	$(target).prop('disabled', true);
+  } else {
+  	$(button).text('Disable Edit');
+  	$(target).prop('disabled', false);
+  }
 }
